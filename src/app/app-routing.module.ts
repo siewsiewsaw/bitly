c40f24656549257bba1b95607f851542d0888d74
siew_siew_saw <siew_siew_saw@dell.com>
2019-09-05 17:01:15 +0800

    bitly master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { ReturnpageComponent } from './returnpage/returnpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "frontpage", component: FrontpageComponent},
+  {path: "returnpage", component: ReturnpageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
